d0997029b9c71687702316197fe4b59b27202709
dulminawijegunarathne <dulminawijegunarathne@gmail.com>
2024-02-07 13:37:54 +0530

    feat: fix line issue

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -4,8 +4,7 @@ import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService: TasksService) {
-  }
+  constructor(private tasksService: TasksService) {}
 
   @Get()
   getAllTasks(): Task[] {
